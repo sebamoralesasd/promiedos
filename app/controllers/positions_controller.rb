6d29627fba7795cad621a1a73a3a70ae1a47b266
Sebastián Morales <sebasuni96@gmail.com>
2023-09-08 20:40:33 -0300

    Fix: el título cambia según el env

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -2,7 +2,7 @@
 
 class PositionsController < ApplicationController
   def index
-    @players = PositionServices::Context.new(PositionServices::Ratio.new).execute
+    @positions = PositionServices::Context.new(PositionServices::Ratio.new).execute
     @match_history = Match.order(created_at: :desc).limit(5)
   end
 end
