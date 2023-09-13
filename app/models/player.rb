7c62b01e701a691818af612f995a7c85729488c4
Sebastián Morales <sebasuni96@gmail.com>
2023-09-12 23:50:09 -0300

    Fix: todavía no está la base de datos nueva

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,7 +3,7 @@
 class Player < ApplicationRecord
   has_many :match_players
   has_many :matches, through: :match_players
-  has_many :tournament_players
-  has_many :tournaments, through: :tournament_players
+  #has_many :tournament_players
+  #has_many :tournaments, through: :tournament_players
   validates :name, presence: true
 end
