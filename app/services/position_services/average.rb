cfe7117b2b26e7f7333deb8886e9991a2dedcc1a
Sebastián Morales <sebasuni96@gmail.com>
2023-10-04 00:08:05 -0300

    Fix: filtrar partidos por torneo

--- app/services/position_services/average.rb
+++ app/services/position_services/average.rb
@@ -10,11 +10,14 @@ module PositionServices
       min_matches_required = max_matches_played ? (max_matches_played / 2) : 0
 
       players = tournament.players
-                          .select('players.*, COUNT(match_players.match_id) AS total_matches, COUNT(matches_winner.id) AS matches_won, ' \
-          'SUM(match_players.points) AS total_points, ' \
-          'CAST(SUM(match_players.points) AS float) / NULLIF(COUNT(match_players.match_id), 0) AS ratio')
+                          .select("players.*,
+                             COUNT(DISTINCT match_players.match_id) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) AS total_matches,
+                             COUNT(matches_winner.id) FILTER(WHERE matches_winner.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) AS matches_won,
+                             SUM(match_players.points) AS total_points,
+                             CAST(SUM(match_players.points) AS float) / NULLIF(COUNT(DISTINCT match_players.match_id) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}), 0) AS ratio")
                           .joins('LEFT JOIN match_players ON match_players.player_id = players.id')
                           .joins('LEFT JOIN matches matches_winner ON matches_winner.id = match_players.match_id AND matches_winner.winner_id = players.id')
+                          .joins('LEFT JOIN matches matches_for_count ON matches_for_count.id = match_players.match_id')
                           .where('matches_winner.tournament_id = ? OR matches_winner.tournament_id IS NULL', tournament.id)
                           .group('players.id')
                           .order(Arel.sql('ratio DESC'))
