e963a13e9bdd6da6ba0caae701db9d32dcea820c
Sebastián Morales <sebasuni96@gmail.com>
2023-10-04 00:36:13 -0300

    Fix: filtro de competencia

--- app/services/position_services/average.rb
+++ app/services/position_services/average.rb
@@ -13,14 +13,16 @@ module PositionServices
                           .select("players.*,
                              COUNT(DISTINCT match_players.match_id) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) AS total_matches,
                              COUNT(matches_winner.id) FILTER(WHERE matches_winner.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) AS matches_won,
-                             SUM(match_players.points) AS total_points,
-                             CAST(SUM(match_players.points) AS float) / NULLIF(COUNT(DISTINCT match_players.match_id) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}), 0) AS ratio")
+                             SUM(match_players.points) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) AS total_points,
+                             CAST(SUM(match_players.points) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) AS float) /
+                             NULLIF(COUNT(DISTINCT match_players.match_id) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}), 0) AS ratio, " \
+        "CASE WHEN COUNT(DISTINCT match_players.match_id) FILTER(WHERE matches_for_count.tournament_id = #{ActiveRecord::Base.connection.quote(tournament.id)}) >= " + min_matches_required.to_i.to_s + ' THEN 1 ELSE 0 END AS eligible_for_tournament')
                           .joins('LEFT JOIN match_players ON match_players.player_id = players.id')
                           .joins('LEFT JOIN matches matches_winner ON matches_winner.id = match_players.match_id AND matches_winner.winner_id = players.id')
                           .joins('LEFT JOIN matches matches_for_count ON matches_for_count.id = match_players.match_id')
                           .where('matches_winner.tournament_id = ? OR matches_winner.tournament_id IS NULL', tournament.id)
                           .group('players.id')
-                          .order(Arel.sql('ratio DESC'))
+                          .order(Arel.sql('eligible_for_tournament DESC, ratio DESC'))
 
       players.map do |player|
         {
@@ -29,8 +31,8 @@ module PositionServices
           total_matches: player.total_matches,
           total_points: player.total_points,
           ratio: player.ratio,
-          eligible_for_tournament: player.total_matches >= min_matches_required,
-          match_history: player.matches.order('created_at DESC').limit(5)
+          eligible_for_tournament: player.eligible_for_tournament == 1 # ,
+          # match_history: player.matches.order('created_at DESC').limit(5)
         }
       end
     end
