4fe375eb98d87a25c1dc4ad4b4b3267f15f75d44
Sebastián Morales <sebasuni96@gmail.com>
2023-10-05 17:54:18 -0300

    Fix: query

--- app/queries/max_matches_played.rb
+++ app/queries/max_matches_played.rb
@@ -1,8 +1,8 @@
 class MaxMatchesPlayed
   def resolve(tournament)
     # Get the maximum matches played by any player
-    Player.joins(:match_players, :matches)
-          .where(matches: { tournament: })
+    Player.joins(match_players: { match: :tournament })
+          .where(tournaments: { id: tournament.id })
           .group('players.id')
           .order('COUNT(match_players.match_id) DESC')
           .first&.match_players&.count
