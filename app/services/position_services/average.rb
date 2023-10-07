5694acf4c3a343ddacf057cf60d1961043fe17d8
Sebastián Morales <sebasuni96@gmail.com>
2023-10-07 12:29:33 -0300

    Fix: puntos totales

--- app/services/position_services/average.rb
+++ app/services/position_services/average.rb
@@ -10,15 +10,16 @@ module PositionServices
       min_matches_required = max_matches_played ? (max_matches_played / 2) : 0
 
       @players = @tournament.players.select('players.*')
-
+      winner_points = params[:winner_points]
       player_stats = []
 
       @players.each do |player|
         player_id = player.id
 
         player_total_matches = total_matches[player_id] || 0
-        player_total_points = total_points[player_id] || 0
         player_matches_won = matches_won[player_id] || 0
+        points = total_points[player_id] || 0
+        player_total_points = points.zero? ? 0 : points + player_matches_won * winner_points
 
         ratio = player_total_matches.zero? ? 0 : player_total_points.to_f / player_total_matches
         eligible_for_tournament = player_total_matches >= min_matches_required.to_i ? 1 : 0
@@ -48,10 +49,15 @@ module PositionServices
     end
 
     def matches_won
-      Match.joins(:match_players)
-           .where(winner_id: @players.map(&:id), tournament_id: @tournament.id)
-           .group(:winner_id)
-           .count
+      a = {}
+      @tournament.players
+                 .select("players.*,
+                    COUNT(matches_winner.id) FILTER(WHERE matches_winner.tournament_id = #{@tournament.id}) AS matches_won")
+                 .joins('LEFT JOIN matches matches_winner ON matches_winner.winner_id = players.id')
+                 .where('matches_winner.tournament_id = :tournament_id OR matches_winner.tournament_id IS NULL', tournament_id: @tournament.id)
+                 .group('players.id')
+                 .each { |p| a[p.id] = p.matches_won }
+      a
     end
 
     def total_points
