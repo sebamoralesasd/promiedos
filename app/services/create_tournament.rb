8e0e94402434e726d2fbf33cc2c4249825762e6b
sebamoralesasd <sebasuni96@gmail.com>
2024-02-18 11:52:52 -0300

    Fix: crear torneo

--- app/services/create_tournament.rb
+++ app/services/create_tournament.rb
@@ -5,7 +5,7 @@ class CreateTournament
     result = nil
     ActiveRecord::Base.transaction do
       start_date = Tournament.last.end_date + 1.day
-      m = Tournament.create!(name: tournament_name, start_date:, end_date: start_date + 3.months)
+      m = Tournament.create!(name: tournament_name, start_date:, end_date: start_date + 3.months - 1.day)
 
       Player.where(name: players_names).each do |p|
         TournamentPlayer.create!(tournament: m, player: p)
