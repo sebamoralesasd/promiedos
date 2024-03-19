d91e7400c35565829293d09fb8aa49e930b18096
sebamoralesasd <sebasuni96@gmail.com>
2024-03-19 18:21:53 -0300

    Fix: numero de fecha

--- app/services/create_match.rb
+++ app/services/create_match.rb
@@ -5,7 +5,7 @@ class CreateMatch
     result = nil
     ActiveRecord::Base.transaction do
       w = Player.find_by!(name: winner_name)
-      m = Match.create!(winner: w, tournament: Tournament.last)
+      m = Match.create!(winner: w, tournament: Tournament.last, number:)
       players.each do |pl|
         name = pl[:name]
         points = pl[:points].to_i
@@ -17,5 +17,12 @@ class CreateMatch
     end
 
     result
+  end
+
+  private
+
+  def number
+    last_match = Match.where(tournament: Tournament.last).last
+    last_match.present? ? last_match.number + 1 : 1
   end
 end
