968febad015ee746d1adf87902d4058819696f7c
sebamoralesasd <sebasuni96@gmail.com>
2024-09-15 14:50:35 -0300

    Fix typo

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -3,6 +3,7 @@
 class MatchPlayer < ApplicationRecord
   belongs_to :player
   belongs_to :match
+  has_one :match_position
 
   validates :points, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
