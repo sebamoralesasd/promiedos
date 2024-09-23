ca511432ba904173e05687b5ffb322ab90399da0
sebamoralesasd <sebasuni96@gmail.com>
2024-09-23 18:03:24 -0300

    Fix controller para crear torneo

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative '../../lib/report_exception'
+
 class TournamentsController < ApplicationController
   def create
     players_names = params[:players_names]
@@ -10,31 +12,19 @@ class TournamentsController < ApplicationController
     create_tournament_service = ::CreateTournament.new
 
     begin
-      match = create_tournament_service.call(players_names, name)
+      match = create_tournament_service.call(players_names, name, 'liga')
       flash[:notice] = 'Tournament created successfully'
       render json: { message: 'Tournament created successfully', match_id: match.id }, status: :created
     rescue ActiveRecord::RecordNotFound => e
-      Rails.logger.error(e.inspect)
-      return unless e.respond_to?(:backtrace) && e.backtrace.present?
-
-      Rails.logger.error(e.backtrace.join("\n"))
-      Rails.logger.error("Player not found: #{e.message}")
+      report_exception(e)
       flash[:alert] = 'Player not found'
       render json: { error: 'Player not found', details: e.message }, status: :not_found
     rescue ActiveRecord::RecordInvalid => e
-      Rails.logger.error(e.inspect)
-      return unless e.respond_to?(:backtrace) && e.backtrace.present?
-
-      Rails.logger.error(e.backtrace.join("\n"))
-      Rails.logger.error("Record Invalid: #{e.record.errors.full_messages}")
+      report_exception(e)
       flash[:alert] = "Record Invalid: #{e.record.errors.full_messages}"
       render json: { error: 'Validation error', details: e.record.errors.full_messages }, status: :unprocessable_entity
     rescue StandardError => e
-      Rails.logger.error(e.inspect)
-      return unless e.respond_to?(:backtrace) && e.backtrace.present?
-
-      Rails.logger.error(e.backtrace.join("\n"))
-      Rails.logger.error("StandardError: #{e.message}")
+      report_exception(e)
       flash[:alert] = 'An error occurred'
       render json: { error: 'An error occurred', details: e.message }, status: :internal_server_error
     end
