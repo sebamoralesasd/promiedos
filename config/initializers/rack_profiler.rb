dd4a3e4c5d044411c15a0dddcd163bf6b4a25af3
sebamoralesasd <sebasuni96@gmail.com>
2025-01-22 19:32:41 -0300

    Fix oculto profiler

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,6 +1,6 @@
-require 'rack-mini-profiler'
-
-Rack::MiniProfilerRails.initialize!(Rails.application)
-
-Rack::MiniProfiler.config.auto_inject = true
-Rack::MiniProfiler.config.authorization_mode = :allow_all
+# require 'rack-mini-profiler'
+#
+# Rack::MiniProfilerRails.initialize!(Rails.application)
+#
+# Rack::MiniProfiler.config.auto_inject = true
+# Rack::MiniProfiler.config.authorization_mode = :allow_all
