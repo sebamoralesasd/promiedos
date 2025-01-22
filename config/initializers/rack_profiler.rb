70517db1a9afd9a2bcb214ab45be960f30c955ac
sebamoralesasd <sebasuni96@gmail.com>
2025-01-22 19:21:49 -0300

    Fix visibilidad

--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -1,3 +1,6 @@
 require 'rack-mini-profiler'
 
 Rack::MiniProfilerRails.initialize!(Rails.application)
+
+Rack::MiniProfiler.config.auto_inject = true
+Rack::MiniProfiler.config.authorization_mode = :allow_all
